f0a5b76bdc72569a0d0dbdd94e5684193dcdf194
sapnk <sapnilkirtan@gmail.com>
2024-05-27 09:45:47 +0530

    added device trees

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_f12.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_f12-user \
+    omni_f12-userdebug \
+    omni_f12-eng
